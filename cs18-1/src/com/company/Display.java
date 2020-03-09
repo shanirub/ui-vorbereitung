166acfcad163b3ef7d80f4e2f77945566aeda8e2
srub <shani.rubovitch@gmail.com>
2020-03-09 11:11:02 +0100

    aufgabe3 done but not working as should

---
+++ cs18-1/src/com/company/Display.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.beans.*;
+import java.lang.annotation.Annotation;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class Display extends JPanel {
+    transient SimpleDateFormat formatter;
+    String date;
+    JLabel label;
+    transient Timer timer;
+
+    public Display() {
+        formatter = new SimpleDateFormat();
+        formatter.format(DateFormat.getDateInstance());
+
+        label = new JLabel();
+        label.setForeground(Color.RED);
+
+        Font font = new Font(Font.SANS_SERIF, Font.BOLD,48);
+        label.setFont(font);
+        label.setText(date);
+
+        JComboBox<String> timezone = new JComboBox<String>();
+        timezone.addItem("Asia/Bangkok");
+        timezone.addItem("Asia/Beirut");
+        timezone.addItem("Asia/Damascus");
+        timezone.addItem("Asia/Istanbul");
+        timezone.addItem("Asia/Jakarta");
+        timezone.addItem("Asia/Shanghai");
+        timezone.addItem("Asia/Tokyo");
+        timezone.addItem("Europe/Berlin");
+        timezone.addItem("Europe/Lisbon");
+        timezone.addItem("Europe/Moscow");
+
+        timezone.setSelectedIndex(7);
+
+        this.setPreferredSize(new Dimension(600,120));
+        this.setBackground(Color.LIGHT_GRAY);
+        this.setBorder(new LineBorder(Color.green, 5));
+        this.setLayout(new BorderLayout());
+
+        this.add(timezone, BorderLayout.SOUTH);
+        this.add(label, BorderLayout.NORTH);
+
+        this.setVisible(true);
+        
+
+    }
+}
